2a67b72bfb059d0695a9d0f9dba362fc2bd5c6bd
Eduard Raimann <e.raimann@gmx.net>
2017-12-24 11:27:51 +0100

    Added comments + improved randomization of ring break

--- app/src/main/java/edray/ringoffire/Card.java
+++ app/src/main/java/edray/ringoffire/Card.java
@@ -3,31 +3,51 @@ package edray.ringoffire;
 /**
  * Created by Ed Ray on 14.10.2017.
  */
+
 public class Card {
 
+    // Array containing all 13 card types
     public int[] type = new int[13];
 
+    // Card gets initialized with either 8 or 13 types, depending on game mode
+    // Every type has 4 different cards (heart, spades, check, cross)
     public Card (int eightOrThirteen){
         for (int i = 0; i<eightOrThirteen; i++){
             this.type[i] = 4;
         }
     }
+
+    // Default function for giving every card a rule
     public String effect(int i){
 
         switch(i) {
+            // 7
             case 0: return "Das wird wehtun! Du darfst 7 Schlucke selbst trinken.";
+            // 8
             case 1: return "Who knows me best? Stelle eine Frage über dich, alle in der Runde notieren ihre Antwort und zeigen sie gleichzeitig. Für jede korrekte Antwort trinkst du, für fast korrekte darf diejenige verteilen und für falsche trinken die entsprechenden Personen.";
+            // 9
             case 2: return "Wähle eine Kategorie. Der Letzte, dem nichts mehr dazu einfällt, muss trinken.";
+            // 10
             case 3: return "Jackpot! Verteile 10 Schlucke an beliebige Personen.";
+            // J
             case 4: return "Zeig den Kommunisten in dir! Nenne ein Anzahl von Schlucken, die jeder inklusive dir trinken muss.";
+            // Q
             case 5: return "Du bist bestimmt wunderschön. Aber ab sofort wird dir niemand mehr in die Augen schauen. Falls doch, so trinkt derjenige.";
+            // K
             case 6: return "Du bist bis zum Zug des nächsten Königs der Fragekönig. Stelle soviele Fragen, wie du willst, dir darf keiner Antworten. Falls doch, trinkt die betreffende Person.";
+            // A
             case 7: return "Mach das Spiel lustiger! Denk dir eine Regel aus, die bei Missachtung zum Trinken führt.";
+            // 2
             case 8: return "Du hast ein Date! Wähle eine Person, die jedes Mal trinken muss, wenn du es tust.";
+            // 3
             case 9: return "Halt den Daumen bereit! Bis zur nächsten 3 darfst du zwischendurch den Daumen auf den Tisch legen. Der letzte, der es checkt, muss trinken.";
+            // 4
             case 10: return "Jetzt sind Rhymeskills angesagt. Nenne ein Wort, der nächste muss darauf reimen.";
+            // 5
             case 11: return "Alle Girls trinken";
+            // 6
             case 12: return "Alle Boys trinken";
+            // Should not happen
             default: return "Hier ist was schief gelaufen.";
         }
     }
--- app/src/main/java/edray/ringoffire/GameActivity.java
+++ app/src/main/java/edray/ringoffire/GameActivity.java
@@ -14,10 +14,14 @@ public class GameActivity extends AppCompatActivity {
     public static final String CARDRADIO = "CardRadio";
     public static final String RULERADIO = "RuleRadio";
     public Card card;
-    public int cards;
-    private int cardCounter = 1;
+    public int cards;  // Number of types
+    public int totalCs;  // Number of total cards
+    public int minBreak;  // Number of cards that must be drawn before the ring is breakable
+    public int maxBreak;  // Number of cards that can be drawn, the last one breaks the ring
+    public double probRange = 1.0;
+    public int randomNum;
+    private int cardCounter = 0;
     private boolean broke = false;
-    private int[] ringBreak = new int[20];
     private String drinkBreak = "";
     public boolean cards32;
     public boolean defRule;
@@ -25,116 +29,104 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);  // Screen is not supposed to be rotated
         setContentView(R.layout.activity_game);
         Intent intent = getIntent();
         cards32 = intent.getExtras().getBoolean(CARDRADIO);
         defRule = intent.getExtras().getBoolean(RULERADIO);
+
+        // Checks options set by user for number of cards
         if (cards32){
             card = new Card(8);
             cards = 8;
+            totalCs = 32;
+            minBreak = 13;  // First possible break = 15
+            maxBreak = 27;  // Maximum possible break = 28
         }
         else {
             card = new Card(13);
             cards = 13;
+            totalCs = 52;
+            minBreak = 23;  // First possible break = 25
+            maxBreak = 47;  // Maximum possible break = 48
         }
     }
 
     public void draw(View v){
-        TextView breakText = (TextView)findViewById(R.id.breakText);
-        TextView cardText = (TextView)findViewById(R.id.cardText);
+        TextView breakText = (TextView)findViewById(R.id.breakText);  // Text showing that the ring was broken
+        TextView cardText = (TextView)findViewById(R.id.cardText);  // Text showing the rule of the card
         TextView debugArray = (TextView)findViewById(R.id.debugArray);
         TextView debugCounter = (TextView)findViewById(R.id.debugCounter);
         TextView debugRandom = (TextView)findViewById(R.id.debugRandom);
         TextView debugNewCard = (TextView)findViewById(R.id.debugNewCard);
 
-        if (cards32) {
-        if (cardCounter == 33) {
-            Intent intent = new Intent(this, GameOverActivity.class);
-            startActivity(intent);
-        }
-        else if (!broke && cardCounter > 4) {
-            if (cardCounter == 25) {
-                broke = true;
-                drinkBreak = "Ring unterbrochen. Du musst exen!";
-            }
-            for (int i = 0; i < cardCounter - 4; i++){
-                int randomNum = randInt(0, 19);
-                ringBreak[i] = randomNum;
-            }
-            int randBreak = randInt(20, 39);
-            for (int i = 0; i < cardCounter - 4; i++) {
-                if ((randBreak - 20) == ringBreak[i]){
-                    broke = true;
-                    drinkBreak = "Ring unterbrochen, sauf!";
-                }
-            }
-        }}
-
-        else {
-            if (cardCounter == 53) {
+            // Last card was drawn, end the game
+            if (cardCounter == totalCs) {
                 Intent intent = new Intent(this, GameOverActivity.class);
                 startActivity(intent);
             }
-            else if (!broke && cardCounter > 8) {
-                if (cardCounter == 45) {
+            if (!broke && cardCounter > minBreak) {
+                // If almost all cards are drawn and the ring still was not broken, break it
+                if (cardCounter == maxBreak) {
                     broke = true;
                     drinkBreak = "Ring unterbrochen. Du musst exen!";
                 }
-                for (int i = 0; i < cardCounter - 8; i++){
-                    int randomNum = randInt(0, 19);
-                    ringBreak[i] = randomNum;
-                }
-                int randBreak = randInt(20, 39);
-                for (int i = 0; i < cardCounter - 8; i++) {
-                    if ((randBreak - 20) == ringBreak[i]){
+                // If the random number is within the probability range, break the ring
+                randomNum = randInt (1,100);
+                for (int i = 1; i <= probRange; i++){
+                    if (randomNum == i){
                         broke = true;
                         drinkBreak = "Ring unterbrochen, sauf!";
                     }
                 }
+                // Raise probability by 1 % or 0.5%
+                if (cards32) {
+                    probRange += 1;
+                }
+                else probRange += 0.5;
             }
-        }
+
+        // Draw a random card
         int randomCard = randInt(0, cards-1);
-        int newCard = check(randomCard, card, cards, cardCounter);
+
+        // Check if the card is still in the game (card.type[randomCard] > 0)
+        int newCard = check(randomCard, card, cards, cardCounter, totalCs);
+
+        // Render the texts to the display
         String effect = card.effect(newCard);
         breakText.setText(drinkBreak);
         cardText.setText(effect);
+
+        // Recalculate (removing used cards from array)
         card.type[newCard] -= 1;
         cardCounter += 1;
         drinkBreak = "";
+
         debugArray.setText(String.valueOf(card.type[randomCard]));
-        debugCounter.setText(String.valueOf(card.type[newCard]));
+        debugCounter.setText(String.valueOf(randomNum));
         debugNewCard.setText(String.valueOf(cardCounter));
+        debugRandom.setText(String.valueOf(probRange));
 
     }
 
-    public int check(int randomCard, Card c, int cs, int cc) {
-        if (cards32) {
-            if (cc == 33) {
+    // Function for checking if the drawn card is still available
+    public int check(int randomCard, Card c, int cs, int cc, int totalCards){
+            if (cc == totalCards) {
                 return 0;
             }
+            // Check the boundaries, if card not available then take the next higher one
+            // If randomCard was the highest then start checking again with the lowest
             else if (c.type[randomCard] == 0) {
                 int ranC = randomCard + 1;
                 if (ranC == cs) {
-                    return check(0, c, cs, cc);
+                    return check(0, c, cs, cc, totalCards);
                 }
-                else return check(ranC, c, cs, cc);
+                else return check(ranC, c, cs, cc, totalCards);
             }
             else return randomCard;
         }
-        else if (cc == 53) {
-            return 0;
-        }
-        else if (c.type[randomCard] == 0) {
-            int ranC = randomCard + 1;
-            if (ranC == cs) {
-                return check(0, c, cs, cc);
-            } else return check(ranC, c, cs, cc);
-        }
-        else return randomCard;
-    }
 
-
+    // Function for generating a random number between min and max (inclusive)
     public static int randInt(int min, int max) {
         Random rand = new Random();
         int random = rand.nextInt((max - min) + 1) + min;
